b846098a9f654cafada881b2a44a75127dc4d066
Aaron Meche <aaronmeche18@icloud.com>
2023-02-26 14:43:57 -0600

    Base

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<!--  -->
+
+<div class="card">
+    <div class="number">4743 3847 2838 3948</div>
+</div>
+
+<!--  -->
+
+<style>
+    .card{
+        margin: 15pt;
+        padding: 15pt;
+    }
+</style>
